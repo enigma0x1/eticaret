c8616e8a0db14f58e0501d202aa88c4649840c4d
Fatih Emre Aksoy <116756659+enigma0x1@users.noreply.github.com>
2024-10-26 01:57:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use((req, res) => {
 });
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
