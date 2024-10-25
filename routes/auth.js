1d03b102456c42fde0f27a793c220b69d04c9ad7
Fatih Emre Aksoy <116756659+enigma0x1@users.noreply.github.com>
2024-10-26 02:35:43 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,8 @@ const verifyToken = require('../middleware/auth');
 // Multer konfigürasyonu
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        // Mutlak yol kullanıyoruz
+        cb(null, path.join(__dirname, '../uploads/'));
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
