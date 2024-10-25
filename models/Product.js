8597f7c3db52ceeebff3116d4470016a6cbd0e59
Fatih Emre Aksoy <116756659+enigma0x1@users.noreply.github.com>
2024-10-26 02:57:03 +0300

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -5,11 +5,20 @@ const productSchema = new mongoose.Schema({
     category: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    image: { type: String, required: true },
+    image: { 
+        type: String, 
+        required: true,
+        get: function(image) {
+            return `/uploads/${image}`;
+        }
+    },
     specs: [String],
     modelFormats: [String],
     stock: { type: Number, required: true },
     rating: { type: Number, default: 0 }
-}, { timestamps: true });
+}, { 
+    timestamps: true,
+    toJSON: { getters: true }
+});
 
 module.exports = mongoose.model('Product', productSchema);
