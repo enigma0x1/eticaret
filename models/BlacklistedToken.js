dd48f372b82c325fa2c1cc9f5d6b633174937944
Fatih Emre Aksoy <116756659+enigma0x1@users.noreply.github.com>
2024-10-26 18:41:52 +0300

    Create BlacklistedToken.js

---
+++ models/BlacklistedToken.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const blacklistedTokenSchema = new mongoose.Schema({
+    token: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 86400 // 24 saat sonra otomatik silinir
+    }
+});
+
+module.exports = mongoose.model('BlacklistedToken', blacklistedTokenSchema);
